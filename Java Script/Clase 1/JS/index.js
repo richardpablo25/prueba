9dfef15397f243cb400263bab185370ec577bbb8
Pablo Richard <richardpablo@hotmail.com>
2022-04-12 20:35:30 -0300

    JS

---
+++ Java Script/Clase 1/JS/index.js
@@ -0,0 +1,18 @@
+/* variables */
+
+var nombre = 'Pablo';
+nombre ='PEPE';
+
+let apellido ='Richard';
+
+const fechaDeNac = '25/07/1966';
+
+/* tipos de datos */
+
+let nombres ="Pablo";    //string//
+let edad =25;   //number//
+let esMayorEdad = true;    // boolean//
+let sexo;   //undefined//
+
+
+
